9879a8f46b9b8256ed3488ae0e47860f1b4ebec5
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-07-27 18:50:09 +0530

    fixed error in params

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -12,7 +12,7 @@ public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
 
-    Piece(final int piecePosition, final Alliance pieceAlliance, int piecePosition1) {
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
         this.pieceAlliance = pieceAlliance;
         this.piecePosition = piecePosition;
     }
