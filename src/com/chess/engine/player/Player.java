fcaf2362305b420c0430b6ca338d7afe2b0929b5
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-09-06 22:06:24 +0530

    added player class, establishKing() and getActivePieces()

--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -1,4 +1,34 @@
 package com.chess.engine.player;
 
-public class Player {
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.King;
+import com.chess.engine.pieces.Piece;
+
+import java.util.Collection;
+
+public abstract class Player {
+
+    protected final Board board;
+    protected final King playerKing;
+    protected final Collection<Move> legalMoves;
+
+    Player(final Board board,
+           final Collection<Move> legalMoves,
+           Collection<Move> opponentMoves) {
+        this.board = board;
+        this.playerKing = establishKing();
+        this.legalMoves = legalMoves;
+    }
+
+    protected King establishKing() {
+        for(final Piece piece: getActivePieces()) {
+            if(piece.getPieceType().isKing()) {
+                return (King) piece;
+            }
+        }
+        throw new RuntimeException("Should not reach here! Not a valid Board!");
+    }
+
+    public abstract Collection<Piece> getActivePieces ();
 }
