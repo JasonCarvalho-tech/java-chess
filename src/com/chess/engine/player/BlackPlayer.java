4547f815f74336de7b37983191ec8725aaddfd16
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-09-06 22:04:37 +0530

    black player class and getActivePieces

--- src/com/chess/engine/player/BlackPlayer.java
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -1,4 +1,23 @@
 package com.chess.engine.player;
 
-public class BlackPlayer {
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.Piece;
+
+import java.util.Collection;
+
+public class BlackPlayer extends Player {
+
+    public BlackPlayer(Board board,
+                       Collection<Move> whiteStandardLegalMoves,
+                       Collection<Move> blackStandardLegalMoves) {
+
+        super(board, blackStandardLegalMoves, whiteStandardLegalMoves);
+
+    }
+
+    @Override
+    public Collection<Piece> getActivePieces() {
+        return this.board.getBlackPieces();
+    }
 }
