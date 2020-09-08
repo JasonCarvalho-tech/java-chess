8a84abb32ab249d5b0c8ae5cf42deff44a728bd9
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-09-08 18:43:26 +0530

    Implemented isInStaleMate()

--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -58,9 +58,8 @@ public abstract class Player {
         return this.isInCheck && !hasEscapeMoves();
     }
 
-    //TODO implement the methods below
     public boolean isInStaleMate() {
-        return false;
+        return !this.isInCheck && !hasEscapeMoves();
     }
 
     protected boolean hasEscapeMoves() {
@@ -73,7 +72,8 @@ public abstract class Player {
         return false;
     };
 
-    public boolean isCasted() {
+    //TODO implement the methods below
+    public boolean isCastled() {
         return false;
     }
 
