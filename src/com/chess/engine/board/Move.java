420f305d460f61889edc1631eaaa4b785e5cace8
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-09-10 19:26:57 +0530

    added skeleton of execute()

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -21,12 +21,21 @@ public abstract class Move {
         return this.destinationCoordinate;
     };
 
+    public abstract Board execute(
+
+    );
+
     public static final class MajorMove extends Move{
 
         public MajorMove(final Board board,
                   final Piece movedPiece,
                   final int destinationCoordinate) {
             super(board, movedPiece, destinationCoordinate);
+        }
+
+        @Override
+        public Board execute() {
+            return null;
         }
     }
 
@@ -40,6 +49,11 @@ public abstract class Move {
                    final Piece attackedPiece) {
             super(board, movedPiece, destinationCoordinate);
             this.attackedPiece = attackedPiece;
+        }
+
+        @Override
+        public Board execute() {
+            return null;
         }
     }
 
