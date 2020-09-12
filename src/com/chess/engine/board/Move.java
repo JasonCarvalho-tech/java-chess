2cc01eacde504743f18fad91c1d4ccf455f88185
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-09-12 15:35:22 +0530

    added concrete classes PawnMove, PawnAttackMove, PawnEnPassantAttackMove, PawnJump, CastleMove, KingSidedCastleMove, QueenSidedCastleMove, NullMove

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -52,7 +52,7 @@ public abstract class Move {
 
     }
 
-    public static final class AttackMove extends Move{
+    public static class AttackMove extends Move{
 
         final Piece attackedPiece;
 
@@ -70,5 +70,69 @@ public abstract class Move {
         }
     }
 
+    public static final class PawnMove extends Move {
+        public PawnMove(final Board board,
+                        final Piece movedPiece,
+                        final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
 
+    public static class PawnAttackMove extends AttackMove {
+        public PawnAttackMove(final Board board,
+                              final Piece movedPiece,
+                              final int destinationCoordinate,
+                              final Piece attackedPiece) {
+            super(board, movedPiece, destinationCoordinate, attackedPiece);
+        }
+    }
+
+    public static final class PawnEnPassantAttackMove extends PawnAttackMove {
+        public PawnEnPassantAttackMove(final Board board,
+                                       final Piece movedPiece,
+                                       final int destinationCoordinate,
+                                       final Piece attackedPiece) {
+            super(board, movedPiece, destinationCoordinate, attackedPiece);
+        }
+    }
+
+    public static final class PawnJump extends Move {
+        public PawnJump(final Board board,
+                                       final Piece movedPiece,
+                                       final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
+
+    static abstract class CastleMove extends Move{
+        public CastleMove(final Board board,
+                         final Piece movedPiece,
+                         final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
+
+    public static final class KingSideCastleMove extends CastleMove {
+        public KingSideCastleMove(final Board board,
+                                  final Piece movedPiece,
+                                  final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
+
+    public static final class QueenSideCastleMove extends CastleMove {
+        public QueenSideCastleMove(final Board board,
+                                   final Piece movedPiece,
+                                   final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
+
+    public static final class NullMove extends Move {
+        public NullMove(final Board board,
+                        final Piece movedPiece,
+                        final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+    }
 }
