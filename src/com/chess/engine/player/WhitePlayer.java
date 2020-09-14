131fd7bb4061a3ddd0509b5beb874dc28d865978
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-09-14 19:56:11 +0530

    introduced code for white's king side castle

--- src/com/chess/engine/player/WhitePlayer.java
+++ src/com/chess/engine/player/WhitePlayer.java
@@ -3,9 +3,13 @@ package com.chess.engine.player;
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class WhitePlayer extends Player {
 
@@ -28,5 +32,23 @@ public class WhitePlayer extends Player {
     @Override
     public Player getOpponent() {
         return this.board.blackPlayer();
+    }
+
+    @Override
+    protected Collection<Move> calculateKingCastles(Collection<Move> playerLegals, Collection<Move> opponentsLegals) {
+        final List<Move> kingCastles = new ArrayList<>();
+
+        if(this.playerKing.isFirstMove() && !this.isInCheck()) {
+            //whites king side castle
+            if(!this.board.getTile(61).isTileOccupied() && !this.board.getTile(62).isTileOccupied()) {
+                final Tile rookTile = this.board.getTile(63);
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()){
+                    //TODO ADD A CASTLEMOVE
+                    kingCastles.add(null);
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(kingCastles);
     }
 }
